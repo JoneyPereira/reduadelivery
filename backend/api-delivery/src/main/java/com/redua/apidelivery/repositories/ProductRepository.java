0af8dcafcb516c4fbbba5016fdaa428dd414b5f6
joney.sousa <joneysousa@yahoo.com.br>
2024-03-01 10:48:12 -0300

    feat: add exeções ao projeto

--- backend/api-delivery/src/main/java/com/redua/apidelivery/repositories/ProductRepository.java
+++ backend/api-delivery/src/main/java/com/redua/apidelivery/repositories/ProductRepository.java
@@ -1,9 +1,7 @@
 package com.redua.apidelivery.repositories;
 
-import com.redua.apidelivery.entities.Order;
 import com.redua.apidelivery.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
