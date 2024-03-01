62d37097af48ea2276a7a6f64bab0929db3fcbb4
joney.sousa <joneysousa@yahoo.com.br>
2024-03-01 11:18:18 -0300

    feat: criado getBy, post, put e delete de produtos

--- backend/api-delivery/src/main/java/com/redua/apidelivery/services/ProductService.java
+++ backend/api-delivery/src/main/java/com/redua/apidelivery/services/ProductService.java
@@ -8,11 +8,14 @@ import com.redua.apidelivery.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -27,13 +30,34 @@ public class ProductService {
         return listProcuts.stream().map(ProductDTO::new).collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAllPaged(Pageable pageable){
+        Page<Product> list = productRepository.findAll(pageable);
+        return list.map(ProductDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(UUID uuid){
+        Optional<Product> obj = productRepository.findById(uuid);
+        Product product = obj.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado, id: " + uuid));
+        return new ProductDTO(product);
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto){
+        Product product = new Product();
+        copyDtoToEntity(dto, product);
+        product = productRepository.save(product);
+        return new ProductDTO(product);
+    }
+
     @Transactional
     public ProductDTO update(UUID uuid, ProductDTO dto){
         try {
-            Product entity = productRepository.getReferenceById(uuid);
-            copyDtoToEntity(dto, entity);
-            entity = productRepository.save(entity);
-            return new ProductDTO(entity);
+            Product product = productRepository.getReferenceById(uuid);
+            copyDtoToEntity(dto, product);
+            product = productRepository.save(product);
+            return new ProductDTO(product);
         }
         catch (EntityNotFoundException e){
             throw new ResourceNotFoundException("Objeto não encontrado, uuid: " + uuid);
@@ -52,9 +76,11 @@ public class ProductService {
         }
     }
 
-    private void copyDtoToEntity(ProductDTO dto, Product entity){
-        entity.setName(dto.getName());
-        entity.setDescription(dto.getDescription());
-        entity.setPrice(dto.getPrice());
+    private void copyDtoToEntity(ProductDTO dto, Product product){
+        product.setName(dto.getName());
+        product.setPrice(dto.getPrice());
+        product.setDescription(dto.getDescription());
+        product.setPrice(dto.getPrice());
+        product.setImageuri(dto.getImageUri());
     }
 }
