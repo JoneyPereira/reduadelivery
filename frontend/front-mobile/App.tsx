05100bf45aff22983c2653f4802c686019645548
JoneySousa <joneysousa@yahoo.com.br>
2024-07-16 16:09:57 -0300

    Add Home page

--- frontend/front-mobile/App.tsx
+++ frontend/front-mobile/App.tsx
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/Header';
+import { OpenSans_400Regular, OpenSans_700Bold, useFonts } from '@expo-google-fonts/open-sans';
+import AppLoading from 'expo-app-loading';
+import Home from './src/Home';
 
 export default function App() {
+  let [fontsLoade] = useFonts({
+    OpenSans_400Regular, OpenSans_700Bold
+  });
+  if (!fontsLoade){
+    return<AppLoading/>;
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Header />
+      <Home />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
 });
